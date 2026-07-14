6b9fd32bbaded6effedbccff151f19fc6e348e86
irrationalpie <irrationalpie7@gmail.com>
2026-07-14 00:20:38 -0700

    Try combining toast and marked

---
+++ docs/marked.js
@@ -0,0 +1,192 @@
+import { Marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.umd.js";
+
+/* [chat pov="Name 1"] [/chat] */
+/* (doesn't support nested tag of same type) */
+function htmlOpenCloseRegex(tag) {
+  if (!isValid(tag)) {
+    console.log(`Attempted to construct match for invalid tag: ${tag}`);
+    return null;
+  }
+  // Opening tag, with non-greedy attributes
+  const openingRegex = `(\\[${tag}.*?\\])`;
+  // Non-greedy innards
+  const middleRegex = `(.+?)`;
+  // Closing tag
+  const endingRegex = `(\\[/${tag}\\])`;
+  return new RegExp(`${openingRegex}${middleRegex}${endingRegex}`, "si");
+}
+
+function isValid(tag) {
+  const validTagRule = /^[a-zA-Z]+$/;
+  const match = rule.exec(text);
+  return !!match;
+}
+
+function parseAttributes(openingTag) {
+  const attrs = {};
+  // quotes can't contain " (non-greedy)
+  const attrRegex = /\s([a-zA-Z]*)="(.*?)"/gs;
+  let match;
+  while ((match = regex.exec(str)) !== null) {
+    attrs[match[1]] = match[2];
+  }
+  return attrs;
+}
+
+const customBlockAttributes = {};
+
+function customBlock(blockName, prelex) {
+  if (!isValid(blockName)) {
+    console.log(`Discarding rule with invalid name: '${blockName}'`);
+    return null;
+  }
+  const blockStart = new RegExp(blockName, "i");
+  return {
+    name: blockName,
+    level: "block",
+    start(src) {
+      return src.match(new RegExp(`\\[${blockName}`, "i"))?.index;
+    },
+    tokenizer(src, tokens) {
+      const rule = htmlOpenCloseRegex(blockName);
+      const match = rule.exec(src);
+      if (match) {
+        const raw = match[0]; // Text to consume from the source
+        const attributes = parseAttributes(match[1], blockName);
+        customBlockAttributes[blockName].attributes = attributes;
+        const innerText = match[2].trim();
+        const rawToken = {
+          // Additional custom properties
+          text: innerText,
+          ...attributes,
+          // Overwrite parsed attrs with required ones
+          type: blockName,
+          raw,
+          tokens: [], // Array where child inline tokens will be generated
+        };
+        const text = preprocess(token.text, token.tokens);
+        this.lexer.blockTokens(text, token.tokens); // Process nested blocks
+        // remove attributes
+        customBlockAttributes[blockName] = undefined;
+        return token;
+      }
+    },
+    renderer(token) {
+      return `<div class=${blockName}>${this.parser.parse(token.tokens)}\n</div>\n`;
+    },
+  };
+}
+
+function textSubType(charName) {
+  const normalized = (charName ?? "").toLowerCase().trim();
+  if (normalized === "info") {
+    return "info";
+  }
+  if (customBlockAttributes["phoneHeader"]?.attributes["pov"]) {
+    const pov = customBlockAttributes["phoneHeader"].attributes["pov"]
+      .split(",")
+      .map((char) => char.trim().toLowerCase());
+    if (pov.includes(normalized)) {
+      return "pov";
+    }
+  }
+  return "character";
+}
+
+const phoneHeader = customBlock("phoneHeader");
+
+// FIXME: use this along with texts?
+/* const multiText = {
+  name: "multiText",
+  level: "block",
+  start(src) {
+    return src.match(/^[^:\n]*?[^\s:]+:\s/m)?.index;
+  },
+  tokenizer(src, tokens) {
+    // character name: text (can be associated with multiple lines,
+    // but gets interrupted by empty line or new character)
+    const rule = /^([^:\n]*?[^\s:]+):(([^:\n]+(?:\n|$))+)/m;
+    const match = rule.exec(src);
+    if (match) {
+      const raw = match[0];
+      // FIXME: return early if charName isn't supported?
+      // Or have a max length?
+      const charName = match[1] ?? textInfo.charName;
+      textInfo.charName = charName;
+      const texts = match[2];
+      // Special casing for "info" type
+      const subType = textSubType(charName);
+      // FIXME: special behavior for pov char?
+      const token = {
+        type: "texts",
+        subType,
+        raw,
+        charName,
+        charDisplay: [],
+        text: texts,
+        tokens: [],
+      };
+      this.lexer.blockTokens(token.text, token.tokens); // Process nested blocks
+      if (type !== "info") {
+        this.lexer.inline(token.charName, token.charDisplay); // Queue charName for inline processing
+      }
+      return token;
+    }
+  },
+}; */
+
+const textInfo = { charName: "" };
+
+const texts = {
+  name: "texts",
+  level: "block",
+  start(src) {
+    return src.match(/^[^:\n]*?[^\s:]+:\s/m)?.index;
+  },
+  tokenizer(src, tokens) {
+    // character name: text (can be associated with multiple lines,
+    // but gets interrupted by empty line or new character)
+    const rule = /^([^:\n]*?[^\s:]+):(([^:\n]+(?:\n|$))+)/m;
+    const match = rule.exec(src);
+    if (match) {
+      const raw = match[0];
+      // FIXME: return early if charName isn't supported?
+      // Or have a max length?
+      const charName = match[1] ?? textInfo.charName;
+      textInfo.charName = charName;
+      const texts = match[2];
+      // Special casing for "info" type
+      const subType = textSubType(charName);
+      // FIXME: special behavior for pov char?
+      const token = {
+        type: "texts",
+        subType,
+        raw,
+        charName,
+        charDisplay: [],
+        text: texts,
+        tokens: [],
+      };
+      this.lexer.blockTokens(token.text, token.tokens); // Process nested blocks
+      if (type !== "info") {
+        this.lexer.inline(token.charName, token.charDisplay); // Queue charName for inline processing
+      }
+      return token;
+    }
+  },
+  renderer(token) {
+    // FIXME: merge character with next token if next token is a paragraph?
+    const normalizedCharName = toCssClass(token.charName);
+    return `<div class="${token.subType} ${normalizedCharName}"><div class="characterName ${normalizedCharName}">${this.parser.parseInline(token.charDisplay)}</div>${this.parser.parse(token.tokens)}\n</div>`; // parseInline to turn child tokens into HTML
+  },
+  childTokens: ["charDisplay", "texts"], // Any child tokens to be visited by walkTokens
+};
+
+// FIXME: be smarter about this?
+function toCssClass(text) {
+  const norm = text.replaceAll(/[^a-zA-Z]/, "-");
+  return `char-${norm}`;
+}
+
+const marked = new Marked([{ extensions: [phoneHeader, texts] }]);
+window.myMarked = marked;
---
+++ docs/toast.js
@@ -0,0 +1,49 @@
+import { Editor } from "https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js";
+const { codeSyntaxHighlight } = Editor.plugin;
+
+function div(entering, className) {
+  return {
+    type: entering ? "openTag" : "closeTag",
+    tagName: "div",
+    outerNewLine: true,
+    ...(entering ? { classNames: [className] } : {}),
+  };
+}
+
+function phonePlugin() {
+  const toHTMLRenderers = {
+    phone(node) {
+      const [header, rest] = node.literal.trim().split("\n", 2);
+      // $$phone [pov: Character, Character Alias 1, Character Alias 2] [heading]
+      const regex = new RegExp("^\\[pov:(.*?)\\](?:\\s+\\[(.*?)\\])?$");
+      const match = regex.exec(header.trim());
+      const rawPOV = match
+        ? match[1].split(",").map((s) => s.replaceAll('"', "'").trim())
+        : [];
+      const rawHeader = match
+        ? (match[2] ?? "").trim()
+        : `Invalid header: '${header.trim()}'`;
+
+      const newLiteral = `[heading pov="${rawPOV.join(",")}"]${rawHeader}[/heading]\n\n${rest}`;
+
+      const html = window.myMarked.parse(newLiteral);
+      // see https://github.com/cure53/DOMPurify#how-do-i-use-it
+      const clean = DOMPurify.sanitize(html);
+
+      return [
+        div(true, "phone"),
+        { type: "html", content: html },
+        div(false, "phone"),
+      ];
+    },
+  };
+  return { toHTMLRenderers };
+}
+
+const editor = new Editor({
+  // Where to put the editor
+  el: document.getElementById("editor"),
+  previewStyle: "vertical",
+  plugins: [phonePlugin, [codeSyntaxHighlight, { highlighter: Prism }]],
+});
+window.editor = editor;
