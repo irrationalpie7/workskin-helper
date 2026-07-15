39487b777ab7af55dcd150e623e20e51d9fed458
irrationalpie <irrationalpie7@gmail.com>
2026-07-14 18:04:34 -0600

    Even moar console.log

--- docs/toast.mjs
+++ docs/toast.mjs
@@ -12,42 +12,39 @@ function div(entering, className) {
   };
 }
 
-function phonePlugin() {
-  const toHTMLRenderers = {
-    phone(node) {
-      const [header, rest] = node.literal.trim().split("\n", 2);
-      // $$phone [pov: Character, Character Alias 1, Character Alias 2] [heading]
-      console.log("about to parse phone:");
-      console.log(header);
-      console.log(rest);
-      const regex = new RegExp("^\\[pov:(.*?)\\](?:\\s+\\[(.*?)\\])?$");
-      const match = regex.exec(header.trim());
-      const rawPOV = match
-        ? match[1].split(",").map((s) => s.replaceAll('"', "'").trim())
-        : [];
-      const rawHeader = match
-        ? (match[2] ?? "").trim()
-        : `Invalid header: '${header.trim()}'`;
+function phone(node) {
+  const [header, rest] = node.literal.trim().split("\n", 2);
+  // $$phone [pov: Character, Character Alias 1, Character Alias 2] [heading]
+  console.log("about to parse phone:");
+  console.log(node);
+  console.log(node.literal);
+  console.log(header);
+  console.log(rest);
+  const regex = new RegExp("^\\[pov:(.*?)\\](?:\\s+\\[(.*?)\\])?$");
+  const match = regex.exec(header.trim());
+  const rawPOV = match
+    ? match[1].split(",").map((s) => s.replaceAll('"', "'").trim())
+    : [];
+  const rawHeader = match
+    ? (match[2] ?? "").trim()
+    : `Invalid header: '${header.trim()}'`;
 
-      const newLiteral = `[heading pov="${rawPOV.join(",")}"]${rawHeader}[/heading]\n\n${rest}`;
+  const newLiteral = `[heading pov="${rawPOV.join(",")}"]${rawHeader}[/heading]\n\n${rest}`;
 
-      console.log("to be parsed:");
-      console.log(newLiteral);
-      const html = marked.parse(newLiteral);
-      // see https://github.com/cure53/DOMPurify#how-do-i-use-it
-      const clean = DOMPurify.sanitize(html);
-      console.log("post parsing:");
-      console.log(html);
-      console.log(clean);
+  console.log("to be parsed:");
+  console.log(newLiteral);
+  const html = marked.parse(newLiteral);
+  // see https://github.com/cure53/DOMPurify#how-do-i-use-it
+  const clean = DOMPurify.sanitize(html);
+  console.log("post parsing:");
+  console.log(html);
+  console.log(clean);
 
-      return [
-        div(true, "phone"),
-        { type: "html", content: clean },
-        div(false, "phone"),
-      ];
-    },
-  };
-  return { toHTMLRenderers };
+  return [
+    div(true, "phone"),
+    { type: "html", content: clean },
+    div(false, "phone"),
+  ];
 }
 
 export function createEditor(element) {
@@ -57,6 +54,7 @@ export function createEditor(element) {
     el: element,
     previewStyle: "vertical",
     initialEditType: "wysiwyg",
-    plugins: [phonePlugin], //, [codeSyntaxHighlight, { highlighter: Prism }]],
+    customHTMLRenderer: { phone },
+    // plugins: [phonePlugin], //, [codeSyntaxHighlight, { highlighter: Prism }]],
   });
 }
